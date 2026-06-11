88d41d239c818a3a2f75092187c679d1cd54486d
somafu <omafuoloche4@gmail.com>
2026-06-11 10:40:29 +0100

    cameltosnake

--- checkpoint/cameltosnake.go
+++ checkpoint/cameltosnake.go
@@ -3,34 +3,57 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func CamelToSnakeCase(s string) string {
 
+	var result strings.Builder
+
 	if s == "" {
 		return ""
 	}
-	var result strings.Builder
 
-	for i, ch := range s {
+	for i, char := range s {
 
-		if ch >= 'a' || ch <= 'z' && ch >= 'A' || ch <= 'Z' {
-
-			if ch >= 'A' || ch <= 'Z' && i > 0 {
-				result.WriteString("_")
-
-			}
-			result.WriteString(string(ch))
-
+		if unicode.IsUpper(char) && unicode.IsUpper(rune(s[i+1])) {
+			return s
 		}
+		if char >= 'A' && char <= 'Z' && i > 0 {
+			result.WriteString("_")
+			result.WriteRune(char + 32)
+			continue
+		}
+		result.WriteRune(char)
 	}
 	return result.String()
 }
 
 func main() {
 
-	fmt.Println(CamelToSnakeCase("samUel"))
+	fmt.Println(CamelToSnakeCase("lowerCamelCase"))
 }
 
+// func CamelToSnakeCase(s string) string {
+// 	if s == ""{
+// 		return ""
+// 	}
+// 	var result strings.Builder
 
-//HelloWor
+// 	for i, ch := range s {
+// 		if unicode.IsUpper(ch) && unicode.IsUpper(rune(s[i+1])){
+// 			return s
+// 		}
+// 		if ch >= 'A' && ch <= 'Z' && i > 0 {
+// 			result.WriteByte('_')
+// 			result.WriteRune(ch+32)
+// 			continue
+// 		}
+
+// 		result.WriteRune(ch)
+// 	}
+
+// 	return result.String()
+// }
+
+// //HelloWor
