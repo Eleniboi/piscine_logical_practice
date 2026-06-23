348d5e275ddc7f3a5b2006d7c0c69e44cb909838
somafu <omafuoloche4@gmail.com>
2026-06-23 09:37:15 +0100

    factorial

---
+++ piscine/factorial.go
@@ -0,0 +1,37 @@
+package main
+
+func IterativeFactorial(nb int) int {
+
+	//this controls negative number and overflow too
+	//factorial of number greater than 12 will not fit into int 34
+	if nb < 0 || nb > 12 {
+		return 0
+	}
+
+	result := 1
+
+	for i := nb; i >= 1; i-- {
+		result *= i
+	}
+
+	return result
+
+}
+
+// func main() {
+
+// 	fmt.Println(IterativeFactorial(5))
+// }
+
+// if nb <= 0{
+// 	return 0
+// }
+
+// var result = 1
+
+// for i := 1; i <= nb; i++ {
+
+// 		result *= i
+// 		fmt.Println(result)
+// }
+// return result
--- piscine/sortinterger.go
+++ piscine/sortinterger.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
-
-//The sortinterTable is a fuction that sort out an unordered numbers into an ordered manner 
+// The sortinterTable is a fuction that sort out an unordered numbers into an ordered manner
 func SortIntegerTable(table []int) {
 
 	//var result = make([]int, len(table))
 
-	for pass := 0; pass < len(table); pass++{
-		for i := 0; i < len(table)-1; i++{
+	for pass := 0; pass < len(table); pass++ {
+		for i := 0; i < len(table)-1; i++ {
 
-			if table[i] > table[i+1]{
+			if table[i] > table[i+1] {
 				temp := table[i]
 				table[i] = table[i+1]
 				table[i+1] = temp
@@ -18,8 +16,9 @@ func SortIntegerTable(table []int) {
 		}
 	}
 }
-func main() {
-	s := []int{5, 4, 3, 2, 1, 0}
-	SortIntegerTable(s)
-	fmt.Println(s)
-}
+
+// func main() {
+// 	s := []int{5, 4, 3, 2, 1, 0}
+// 	SortIntegerTable(s)
+// 	fmt.Println(s)
+// }
